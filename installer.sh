876d0725a5cf395edb16413cd5fa8844b24d0874
Mario Dujić <mario.dujic@gmail.com>
2022-09-30 19:45:38 +0200

    Add installer

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+askInstall() {
+  read -p "Would you like to install Pages [Y/n]" -r answer
+  if [[ "$answer" = [Yy] ]]; then
+    echo "Installing Pages"
+    chmod a+x pages/pages
+    cp pages/pages /usr/local/bin >/dev/null 2>&1 || echo "Error, try running as sudo."
+    exit 1
+  elif [[ "$answer" != [Nn] ]]; then
+    echo "Unknown answer, please try again."
+    askInstall
+  fi
+}
+
+askInstall
