9f6257b7de5d9551ffc8bf4069b4a88f384e3cb8
Mario Dujić <mario.dujic@gmail.com>
2022-10-17 16:44:25 +0200

    Add CI

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ askInstall() {
     echo "Installing Pages"
     chmod a+x pages/pages
     cp pages/pages /usr/local/bin >/dev/null 2>&1 || echo "Error, try running as sudo."
-    exit 1
+    exit 0
   elif [[ "$answer" != [Nn] ]]; then
     echo "Unknown answer, please try again."
     askInstall
